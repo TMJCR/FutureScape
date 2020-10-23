e41240cb043d6ac9c687d72e9eb843ec41f3f319
ThomasC <thomascrowdy@gmail.com>
2020-10-23 23:28:07 +0200

    Refactored for responsive mobile

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,13 +3,14 @@ import '../css/ProjectCard.css';
 import ProjectInfo from './ProjectInfo';
 import VideoPlayer from './VideoPlayer';
 export default function ProjectCard({ Project }) {
+  const baseUrl = require(`../images/${Project.video}_Moment.jpg`);
   return (
-    <section id="Projects">
+    <section id="Projects" className="Card">
       <div className="ProjectCardContainer">
         <div className="ProjectCardInternal ProjectCardModel">
           <ProjectInfo name={Project.name} country={Project.country} city={Project.city} height={Project.height} floors={Project.floors} Function={Project.function} building={Project.building}></ProjectInfo>
         </div>
-        <div id="video" className="ProjectCardInternal ProjectCardVideo">
+        <div id="video" className="ProjectCardInternal ProjectCardVideo" style={{ backgroundImage: { baseUrl } }}>
           <VideoPlayer videoFile={Project.video}></VideoPlayer>
         </div>
       </div>
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function VideoPlayer({ videoFile }) {
-  const baseUrl = require(`../images/${videoFile}`);
+  const baseUrl = require(`../images/${videoFile}.mp4`);
   return (
-    <video key={videoFile} playsInline webkit-playsinline="true" loop autoPlay muted width="500">
+    <video key={videoFile} playsInline webkit-playsinline="true" loop autoPlay muted width="600">
       <source src={baseUrl} type="video/mp4"></source>
       Sorry, your browser doesn't support embedded videos.
     </video>
