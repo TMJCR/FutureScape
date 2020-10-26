2c7f5db9ea922f7ba3e4e971a34abb39f0601008
ThomasC <thomascrowdy@gmail.com>
2020-10-26 13:11:47 +0100

    Testing for responsive mobile

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VideoPlayer({ videoFile }) {
   const baseUrl = require(`../images/${videoFile}.mp4`);
   return (
-    <video key={videoFile} playsInline webkit-playsinline="true" loop autoPlay muted width="600">
+    <video key={videoFile} playsInline webkit-playsinline="true" loop autoPlay muted>
       <source src={baseUrl} type="video/mp4"></source>
       Sorry, your browser doesn't support embedded videos.
     </video>
